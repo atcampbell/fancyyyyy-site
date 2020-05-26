e8f977ca65fe29d06f23cc2cdb74185edefacc8f
atcampbell <adamtoddcampbell@gmail.com>
2020-05-26 17:39:41 +0100

    remove aws-exports import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
-import Amplify from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+// TODO build on aws cloud is failing to generate aws-exports
+// Followed this but not resolved...
+// https://github.com/aws-amplify/amplify-cli/issues/754
+
+// import Amplify from 'aws-amplify';
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
