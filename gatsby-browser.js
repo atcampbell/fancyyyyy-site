6ab00b5a5084bc5dd4b2c419dad73c23f851ed7a
adam campbell <adamtoddcampbell@gmail.com>
2024-08-19 23:24:32 +0200

    uncomment amplify configure

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
-// TODO build on aws cloud is failing to generate aws-exports
-// Followed this but not resolved...
-// https://github.com/aws-amplify/amplify-cli/issues/754
-
-// import Amplify from 'aws-amplify';
-// import awsExports from './aws-exports';
-// Amplify.configure(awsExports);
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
